73bb96db4bf58d8896f2cdb3213798e8afdd83f9
CosteaMihai <57099450+CosteaMihai@users.noreply.github.com>
2021-11-26 16:52:33 +0200

    implemented cold-start with dummy data

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ColdStartComponent } from './feature/cold-start/cold-start.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ColdStartComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
