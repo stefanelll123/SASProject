b6bba366858b054f465e6987ab036d4a3a9d0fcd
Stefan Turcu <stefan.turcu@selfpay.ro>
2022-01-15 18:05:47 +0200

    Recommendation

--- backend/recommendation/app.py
+++ backend/recommendation/app.py
@@ -1,10 +1,18 @@
 import os
+import jwt
+import json
 
 from flask import Flask
 from flask_cors import CORS, cross_origin
 from flask import request
+from flask import Response
+
+import requests
+
 from pymongo import MongoClient
 from dotenv import load_dotenv
+
+from newsfeedResponse import NewsfeedResponse
 
 app = Flask(__name__)
 CORS(app)
@@ -14,9 +22,56 @@ client = MongoClient(os.getenv('CONNECTION_STRING'))
 db = client['recommendation']
 articles = db['articles']
 
-@app.route("/api/articles", methods = ['GET'])
+key = os.getenv('SECRET')
+identityUri = os.getenv('IDENTITY_URI')
+
+def isValidToken(token):
+    try:
+        decodeJwt(token)
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+def decodeJwt(token):
+    components = token.split(' ')
+    if len(components) != 2:
+        raise Exception()
+
+    token = components[1]
+
+    header_data = jwt.get_unverified_header(token)
+    return jwt.decode(token, key=key, algorithms=[header_data['alg'], ])
+
+@app.before_request
+def checkIfAuthentificated():
+    response = Response('{"error": "Please login before"}', status=401)
+    if request.headers.get('Authentication') == None:
+        return response
+
+    if not isValidToken(request.headers.get('Authentication')):
+        return response
+        
+@app.route("/api/newsfeed", methods = ['GET'])
 @cross_origin()
 def getArticlesForUser():
-    pass
+    userId = decodeJwt(request.headers.get('Authentication'))['sub']
+    
+    response = requests.get(identityUri + '/api/users/%s/preferences' % userId)
+    if response.status_code == 404:
+        return Response('{"error": "Userid not found"}', status=404)
+
+    if response.status_code != 200:
+        return Response('{"error": "%s"}' % response.reason, status=response.status_code)
+
+    preferences = json.loads(response.content)['preferences']
+    preferences = [x.lower() for x in preferences]
+    
+    queryParams = request.args.to_dict()
+    offeset = 0 if queryParams.get('offeset') == None else int(queryParams.get('offeset'))
+    limit = 20 if queryParams.get('limit') == None else int(queryParams.get('limit'))
+
+    results = articles.find({ 'tags': { '$in': preferences } }).skip(offeset).limit(limit)
+    return Response(str(NewsfeedResponse(list(results))), status=200)
 
 app.run(debug=True)
---
+++ backend/recommendation/newsfeedResponse.py
@@ -0,0 +1,21 @@
+import json
+
+class NewsfeedResponse():
+    def __init__(self, articles) -> None:
+        self.articles = []
+        for article in articles:
+            self.articles.append(Article(article).__dict__)
+
+    def __str__(self) -> str:
+        return json.dumps(self.__dict__)
+
+class Article():
+    def __init__(self, article) -> None:
+        self.id = str(article['_id'])
+        self.title = article['title']
+        self.subtitle = article['subtitle']
+        self.source = article['source']
+        self.cover = 'https://miro.medium.com/max/875/' + article['cover']
+
+    def __str__(self) -> str:
+        return json.dumps(self.__dict__)
